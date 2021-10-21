f3a8b907b951622f83776e33e6c6d2271d3b728b
carbotton <majocarbajal@gmail.com>
2021-10-21 15:34:33 -0300

    codigo servo

--- bondiot-nodemcu-arduino/bondiot-nodemcu-arduino.ino
+++ bondiot-nodemcu-arduino/bondiot-nodemcu-arduino.ino
@@ -22,6 +22,7 @@
 #include <PubSubClient.h> 
 #include <ArduinoJson.h>
 #include <string.h>
+#include <Servo.h>
 
 #include "sniffer_functions.h"
 #include "iot_functions.h"
@@ -63,11 +64,12 @@
 #define RECEVIE_TIMEOUT 5000    // Time in milliseconds
 
 // ---------- DEFINE PINS ----------
-#define MQ2_PIN A0
-#define LOADCELL_DOUT_PIN D5
-#define LOADCELL_SCK_PIN D6
-#define FRONTDOOR_OUT_PIN 7
-#define BACKDOOR_OUT_PIN 8
+#define MQ2_PIN A0              //co2 analog
+#define LOADCELL_DOUT_PIN D5    //loadcell data
+#define LOADCELL_SCK_PIN D6     //loadcell clock
+#define FRONTDOOR_OUT_PIN 7     //IR frontdoor
+#define BACKDOOR_OUT_PIN 8      //IR backdoor
+#define SERVO D4                //servo
 
 // =====================================
 //            GLOBAL VARIABLES
@@ -77,6 +79,7 @@ int clients_known_count_old, aps_known_count_old;
 unsigned long sendEntry, deleteEntry;
 char jsonStringOut[JBUFFER];
 char* jsonStringIn;
+//StaticJsonBuffer<JBUFFER>  jsonBuffer;
 
 String device[MAXDEVICES];
 int nbrDevices = 0;
@@ -89,16 +92,17 @@ bool WiFi_OK = false;
 bool TB_OK = false;
 
 HX711 scale;
+Servo myServo;
+int openedPos = 90;
+int closedPos = 0;
 String calibrationMode = "ON";		 // !!!!!!!! ESTO TIENE QUE TRAERSE DE THINGSBOARD
+String servoState = "OPEN";	// !!!!!!!! ESTO TIENE QUE TRAERSE DE THINGSBOARD
 unsigned int loadcell_timeout = 1000; // !!!!!!!! ESTO TIENE QUE TRAERSE DE THINGSBOARD
 unsigned int weight_variation = 10;  // !!!!!!!! ESTO TIENE QUE TRAERSE DE THINGSBOARD
 unsigned int weight_for_calibration = 1;  //Kg !!!!!!!! ESTO TIENE QUE TRAERSE DE THINGSBOARD
 unsigned int last_weight = 0;
 unsigned int passengers = 0;
 float calibration_constant = 1.f;
-
-//StaticJsonBuffer<JBUFFER>  jsonBuffer;
-
 
 // =====================================
 //            INTERRUPT FUNCTIONS
@@ -121,41 +125,50 @@ void read_backdoor()
 // =====================================
 void setup() {
   Serial.begin(115200);
-  if (WIFI_DEBUG == 1){
-    debugf("\n\n\n\n\n\nBondIoT - version: 1.1\n",NULL);
-    debugf("SDK version: %s\n\n", system_get_sdk_version());
-    debugln("========================");
-    debugln("   DEBUG MODE ENABLED   ");
-    debugln("========================");
-    debugf("\n\n",NULL);
-    debugln("Please input access point SSID");
-    while(!Serial.available()) delay(1);
-    wifi_SSID = Serial.readString();
-    debugln("Please input access point PASSWORD");
-    while(!Serial.available()) delay(1);
-    wifi_PASSWORD = Serial.readString();
-    debugf("Credentials saved successfully!\n\n\n\n\n",NULL);
-  }
 
-  wifi_set_opmode(STATION_MODE);            // Promiscuous works only with station mode
-  wifi_set_channel(channel);
-  wifi_promiscuous_enable(disable);
-  wifi_set_promiscuous_rx_cb(promisc_cb);   // Set up promiscuous callback
-  wifi_promiscuous_enable(enable);
+  // ---------- debug mode ----------
+    if (WIFI_DEBUG == 1){
+      debugf("\n\n\n\n\n\nBondIoT - version: 1.1\n",NULL);
+      debugf("SDK version: %s\n\n", system_get_sdk_version());
+      debugln("========================");
+      debugln("   DEBUG MODE ENABLED   ");
+      debugln("========================");
+      debugf("\n\n",NULL);
+      debugln("Please input access point SSID");
+      while(!Serial.available()) delay(1);
+      wifi_SSID = Serial.readString();
+      debugln("Please input access point PASSWORD");
+      while(!Serial.available()) delay(1);
+      wifi_PASSWORD = Serial.readString();
+      debugf("Credentials saved successfully!\n\n\n\n\n",NULL);
+    }
+  //-
   
-	/*// ---------- interruptions ----------
-	pinMode(FRONTDOOR_OUT_PIN, INPUT_PULLUP);
-	pinMode(BACKDOOR_OUT_PIN, INPUT_PULLUP);
-	attachInterrupt(FRONTDOOR_OUT_PIN, read_frontdoor, RISING);
-	attachInterrupt(BACKDOOR_OUT_PIN, read_backdoor, RISING);
-	//-	 
-	*/
-  scale = setUpLoadCell(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);  
-	if (calibrationMode.equals("ON")){
-		calibration_constant = calibrateLoadCell(scale, weight_for_calibration);
-    scale.set_scale(calibration_constant);
-	}
-	delay(2000); //co2 warm-up
+  // ---------- wifi ----------
+    wifi_set_opmode(STATION_MODE);  // Promiscuous works only with station mode
+    wifi_set_channel(channel);
+    wifi_promiscuous_enable(disable);
+    wifi_set_promiscuous_rx_cb(promisc_cb); // Set up promiscuous callback
+    wifi_promiscuous_enable(enable);
+  //-
+  
+  // ---------- interruptions ----------
+  	pinMode(FRONTDOOR_OUT_PIN, INPUT_PULLUP);
+  	pinMode(BACKDOOR_OUT_PIN, INPUT_PULLUP);
+  	attachInterrupt(FRONTDOOR_OUT_PIN, read_frontdoor, RISING);
+  	attachInterrupt(BACKDOOR_OUT_PIN, read_backdoor, RISING);
+  //-	 
+
+  // ---------- servo, scale, co2 ----------
+    myServo.attach(SERVO);
+    scale = setUpLoadCell(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);  
+  	if (calibrationMode.equals("ON")){
+  		calibration_constant = calibrateLoadCell(scale, weight_for_calibration);
+      scale.set_scale(calibration_constant);
+  	}
+  	delay(2000); //co2 warm-up
+  //-
+
 }
 
 
@@ -163,6 +176,7 @@ void setup() {
 //                 LOOP
 // =====================================
 void loop() {
+  
   channel = 1;
   wifi_set_channel(channel);
   
@@ -179,41 +193,23 @@ void loop() {
     purgeDevice(PURGETIME);                 // Delete old devices that are no longer in range
     
   }//endwhile
-  
-	//read CO2 level
+
+  //read co2 and loadcell sensors
 	unsigned int readingCO2 = read_co2(MQ2_PIN);
-  
-	//read weight and send to thingsboard only when there is a significant variation
-	String readingLoadCellStr = read_weight(scale, loadcell_timeout);
+  String readingLoadCellStr = read_weight(scale, loadcell_timeout);
 
-  //check if weight variation was enough to be considered
-  /*String readingLoadCellOK = "";
-	int readingLoadCell = readingLoadCellStr.toInt(); 
-	if (readingLoadCell != 0){		
-		if (readingLoadCell-last_weight >= weight_variation){ //if enough variation send reading else ...
-			readingLoadCellOK = readingLoadCellStr;
-		}			
-		last_weight = readingLoadCell;
-	}else{
-    Serial.print("Reading load cell: ");
-		Serial.println(readingLoadCell);
-	}*/
-
-	//create and send json
   if (millis() - sendEntry > SENDTIME) {
     sendEntry = millis();
-    //showDevices();  //Prints MAC addressses to the serial monitor
-    
-    //jsonString = generateJson();
-    
-    wifi_promiscuous_enable(disable);   // Disable promiscuous mode in order to connect to the access point
-           
-    WiFi_OK = connectToWiFi(wifi_SSID, wifi_PASSWORD, WiFi_connect_attempts);   // Connect to WiFi access point
-    
-    if (WiFi_OK) TB_OK = connectToThingsBoard(TB_SERVER, NODE_NAME, NODE_TOKEN, NODE_PW, TB_connect_attempts);    // If WiFi connected successfully, connect to ThingsBoard
-  
-    topic = telemetryTopic;
+    //showDevices();  //Prints MAC addressses to the serial monitor    
+    //jsonString = generateJson();  
 
+    // Disable promiscuous mode in order to connect to the access point 
+    wifi_promiscuous_enable(disable);             
+    WiFi_OK = connectToWiFi(wifi_SSID, wifi_PASSWORD, WiFi_connect_attempts);   // Connect to WiFi access point    
+    if (WiFi_OK) 
+      TB_OK = connectToThingsBoard(TB_SERVER, NODE_NAME, NODE_TOKEN, NODE_PW, TB_connect_attempts);    // If WiFi connected successfully, connect to ThingsBoard 
+
+    //create json to send to thingsboard
     String test = "{'co2': "; 
     test += String(readingCO2);
     test += ", 'loadcell': ";
@@ -223,8 +219,9 @@ void loop() {
   
     char payload[100];
     test.toCharArray(jsonStringOut, JBUFFER);
-          
-    if (WiFi_OK && TB_OK) sendValues(topic, jsonStringOut);    // If connected, send data to ThingsBoard
+    topic = telemetryTopic;              
+    if (WiFi_OK && TB_OK) 
+      sendValues(topic, jsonStringOut);    // If connected, send data to ThingsBoard
   
     //jsonStringIn = receiveData(topic, RECEVIE_TIMEOUT);
   
@@ -232,7 +229,12 @@ void loop() {
     WiFi.disconnect();    // Disconnect from WiFi
     wifi_promiscuous_enable(enable);    // Re-enable promiscuous mode
   
-  }  
-  //thingsboard communication
-  //si se recibieron instrucciones ejecutar accion, else vuelvo arriba a medir CO2  
+  }//end if sendtime
+
+  //move servo
+  if (servoStatus.equals("OPEN"))
+    if (servo.read() == closedPos) myServo.write(openedPos);
+  else
+    if (servo.read() == openedPos) myServo.write(closedPos);
+      
 }
